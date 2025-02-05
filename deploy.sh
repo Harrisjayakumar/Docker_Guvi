903cd3b0117b2e06068a4750643894b9fb34be0b
Harrisjayakumar V <111297556+Harrisjayakumar@users.noreply.github.com>
2025-02-05 11:54:03 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u harrisjayakumar -p Harri$22@05
+    docker login -u harrisjayakumar -p 123456789
     docker tag test harrisjayakumar/task2.2
     docker push harrisjayakumar/task2.2
     
